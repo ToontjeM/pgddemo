e5cc63d56331b3d050b55e870e996cdb03be711d
Ton Machielsen--replace-all <ton@machielsen.net>
2024-09-30 09:30:48 +0200

    Final

--- 03_drop_node.sh
+++ 03_drop_node.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-# Drop node2
-psql bdrdb -c "select bdr.part_node('node2');"
-psql bdrdb -c "select bdr.drop_node('node2');"
+# Drop node
+
+nodeid=$1
+
+if [ `whoami` != "enterprisedb" ]
+then
+  printf "You must execute this as enterprisedb\n"
+  exit
+fi
+
+if [ -z "$nodeid" ]; then
+  echo "NodeId can not be empty"
+  exit
+fi
+
+psql bdrdb -c "select bdr.part_node('node$1');"
+psql bdrdb -c "select bdr.drop_node('node$1');"
